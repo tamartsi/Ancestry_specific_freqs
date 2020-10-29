a574ab93a6b14eee1e8ea76dbad9256a8bafb2cd
Tamar Sofer <tamartsi@users.noreply.github.com>
2020-10-28 22:17:42 -0400

    added smoothing data
    Also added some documentation

--- estimate_frequencies_w_known_freqs.R
+++ estimate_frequencies_w_known_freqs.R
@@ -6,11 +6,15 @@
 # known_freq need to be a named vector, specifying known frequencies. The names of elements 
 # in known_freq are some of the column names of prop_mat. 
 estimate_frequencies_w_known_freqs <- function(allele_counts, prop_mat, known_freqs, confidence = 0.95, 
-                                            low_freq_bound = 0.001, high_freq_bound = 0.999){
+                                            low_freq_bound = 0.001, high_freq_bound = 0.999,
+                                            use_smoothing_data = FALSE){
+  
   stopifnot(length(allele_counts) == nrow(prop_mat))
   stopifnot(all(is.element(names(known_freqs), colnames(prop_mat))))
   stopifnot(length(known_freqs) > 0 & length(known_freqs) < ncol(prop_mat))
-  
+
+  # turn each allele dosage into a two rows, each reporting one allele
+  # with the same proportion ancestries.  
   decomposed_alleles <- sapply( allele_counts,decompose_two_alleles_one_person)
   decomposed_alleles <- as.numeric(matrix(decomposed_alleles)	)
   
@@ -22,6 +26,17 @@ estimate_frequencies_w_known_freqs <- function(allele_counts, prop_mat, known_fr
   inds_unknown <- setdiff(1:ncol(prop_mat), inds_known)
   prop_mat <- prop_mat[,c(inds_known, inds_unknown)]
   prop_mat_double <- prop_mat[rep(1:nrow(prop_mat), each = 2),]
+
+  # add made-up data to avoid boundaries of the frequency parameter space 
+  if (use_smoothing_data){
+    smoothing_data <- .generate_smoothing_observations(colnames(prop_mat))
+    # add only the rows corresponding to the ancestries with unknown frequencies
+    inds_keep <- (n_known*2 + 1):((n_known + n_unknown)*2)
+    prop_mat <- rbind(prop_mat_double, smoothing_data$simulated_prop_mat[inds_keep,])
+    allele_counts <- c(decomposed_alleles, smoothing_data$simulated_allele_count[inds_keep])
+  }
+  
+  
   
   known_sums <- prop_mat_double[,names(known_freqs), drop = FALSE] %*% known_freqs
   

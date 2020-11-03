ea5b2cdfbe3b7ed2b65c48b3a649a3d02bd46622
Tamar Sofer <tamartsi@users.noreply.github.com>
2020-11-03 06:53:21 -0500

    Added chromosome X details
    added to chromosome X flag, sex, and male lable. If chromosome X data, male alleles treated differently than female's.

--- tests_and_examples.R
+++ tests_and_examples.R
@@ -21,3 +21,26 @@ estimate_frequencies_w_known_freqs(allele_count, prop_mat, known_freqs = c(eur=0
 
 
 estimate_frequencies_w_known_freqs(allele_count, prop_mat, known_freqs = c(eur=0.1, afr = 0.8))
+
+
+# check X chromosome: 
+prop_mat <- matrix(c(0.1, 0.7, 0.2, 0.2, 0.2, 0.6, 0.4, 0.2, 0.4, 0.2, 0.3, 0.5, 0.6, 0.2, 0.2, 1, 0, 0, 0, 1, 0, 0.1, 0.8, 0.1), nrow = 8, byrow = TRUE)
+colnames(prop_mat) <- c("eur", "afr", "amr")
+rowSums(prop_mat) # all are 1. 
+true_freqs <- c(eur =0.7, afr = 0.8, amr = 0.2)
+probs <- prop_mat %*% true_freqs
+allele_count <- rbinom(length(probs), 2, probs )
+sex <- c("f", "m", "f", "f", "f", "m", "f", "f")
+estimate_frequencies(allele_count, prop_mat,
+                     low_freq_bound = 0.05, 
+                     high_freq_bound = 0.95, 
+                     chromosome_x = TRUE,
+                    sex = sex, 
+                    male_label = "m")
+# if we don't provide sex...
+estimate_frequencies(allele_count, prop_mat,
+                     low_freq_bound = 0.05, 
+                     high_freq_bound = 0.95, 
+                     chromosome_x = TRUE,
+                     male_label = "m")
+# error! (as should be)

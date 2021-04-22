e6f10b9c6bfedef87cefb4267df80d62fa986504
Tamar Sofer <tamartsi@users.noreply.github.com>
2021-04-22 07:05:41 -0400

    updated return values, improved function
    Now the function returns a list with frequencies, negative log likelihood, boundary used in estimation, and "flag" providing estimation status.

--- estimate_frequencies_search_boundary.R
+++ estimate_frequencies_search_boundary.R
@@ -14,7 +14,8 @@ estimate_frequencies_search_boundary <- function(allele_counts, prop_mat, confid
                                  male_label = "M", 
                                  mac_filter = 5){
   
-  # prepare data.frame wor return null results if the function didn't ran
+  # prepare data.frame for returning null results if the function doesn't run
+  # or does not converge
   return_val_not_run <- data.frame(ancestry = colnames(prop_mat),
                                    estimated_freq = NA,
                                    low_CI = NA,
@@ -33,7 +34,10 @@ estimate_frequencies_search_boundary <- function(allele_counts, prop_mat, confid
 
   
   if (min(sum(allele_counts), sum(max_counts) - sum(allele_counts)) <= mac_filter){
-    return(return_val_not_run)
+    return(list(res = return_val_not_run, 
+                nll = NA,
+                boundary = NA, 
+                flag = "MAC filter"))
   }
   
   # start loop to find boundaries in which the analysis converges:
@@ -64,38 +68,74 @@ estimate_frequencies_search_boundary <- function(allele_counts, prop_mat, confid
   }
   
   
-  if (!converged) return(return_val_not_run)
+  if (!converged) {
+    return(list(res = return_val_not_run, 
+                nll = NA,
+                boundary = NA, 
+                flag = "Not converged"))
+  } else{ # if converged, prepare potential return value
+    potential_return_val_converged <- list(res = cur_res$res,
+                                           nll = cur_res$nll,
+                                           boundary = low_freq_bound,
+                                           flag = "converged")
+  }
   
-  # check if convergence is right at the boundary condition
+  # check if convergence is right at the boundary condition for some ancestries
   low_bound_inds <- which(cur_res$res$estimated_freq == low_freq_bound)
   high_bound_inds <- which(cur_res$res$estimated_freq == high_freq_bound)
   
-  if (length(low_bound_inds) > 0 | length(high_bound_inds) > 0){
+  # if nothing was estimated at the boundary, return the computed frequencies.
+  if (length(low_bound_inds) + length(high_bound_inds) == 0){
+    return(potential_return_val_converged)
+  }
+  
+  # otherwise, we continue to identify which frequencies were 
+  # estimated at the boundaries and...
+  # prepare a vector called set_freqs with in which the ancestries
+  # where frequencies were estimated in the boundaries are set to the 
+  # boundaries of the parameter space. Start with low and then high boundary.
+    
   # if frequencies for some ancestries were estimated right at the lower bound:
   if (length(low_bound_inds) >0){
-    known_freqs <- rep(0, length = length(low_bound_inds))
-    names(known_freqs) <- res$ancestry[low_bound_inds]
-  }
+      set_freqs <- rep(0, length = length(low_bound_inds))
+      names(set_freqs) <- res$ancestry[low_bound_inds]
+    }
   
   # if frequencies for some ancestries were estimated right at the high bound:
   if (length(high_bound_inds) >0 ){
-    known_freqs_high <- rep(1, length = length(high_bound_inds))
-    names(known_freqs_high) <- res$ancestry[high_bound_inds]
+      set_freqs_high <- rep(1, length = length(high_bound_inds))
+      names(set_freqs_high) <- res$ancestry[high_bound_inds]
+      
+      # should we augment a previous vector, and have a new set_freqs vector?
+      if (length(low_bound_inds) > 0){
+        set_freqs <- c(set_freqs, set_freqs_high)
+      } else{
+        set_freqs <- set_freqs_high
+      }
+    } # finished preparing a vector of set frequencies
+ 
+  # first: if all frequencies were estimated at the boundary:
+  if (length(set_freqs) == ncol(prop_mat)){
+    allele_probs <- prop_mat[,names(set_freqs), drop = FALSE] %*% set_freqs
+    nll_by_obs <- log(dbinom_approx(allele_counts, max_counts, allele_probs))
+    nll <- -sum(nll_by_obs)
     
-    # should we augment a previous vector, and have a new known_freqs vector?
-    if (length(low_bound_inds) > 0){
-      known_freqs <- c(known_freqs, known_freqs_high)
+    if (nll < potential_return_val_converged$nll){
+      return_val <- return_val_not_run
+      return_val[match(names(set_freqs), return_val$ancestry),"estimated_freq"] <- set_freqs
+      return(list(res = return_val,
+                  nll = nll,
+                  boundary = potential_return_val_converged$boundary,
+                  flag = "converged and evaluated monomorphic"))
     } else{
-      known_freqs <- known_freqs_high
+      return(potential_return_val_converged)
     }
   }
   
-  # if some frequencies were estimated right on the provided boundaries, 
-  # check if the likelihood is actually maximized at the boundary of the 
-  # parameter space.
-  if (length(known_freqs) > 0){
-    boundary_res <- estimate_frequencies_w_known_freqs(allele_counts, prop_mat, 
-                                                       known_freqs, confidence, 
+  # at this point: some frequencies but not all were estimated at the boundary
+   
+  boundary_res <- estimate_frequencies_w_known_freqs(allele_counts, prop_mat, 
+                                                       set_freqs, confidence, 
                                                        low_freq_bound = frequency_boundary_grid[1], 
                                                        high_freq_bound = 1-frequency_boundary_grid[1],
                                                        use_smoothing_data = use_smoothing_data,
@@ -109,13 +149,17 @@ estimate_frequencies_search_boundary <- function(allele_counts, prop_mat, confid
       # frequency for the ancestry with boundary value set to that value
       return_val <- return_val_not_run
       return_val[match(boundary_res$res$ancestry, return_val$ancestry),] <- boundary_res$res
-      return_val[match(names(known_freqs), return_val$ancestry),"estimated_freq"] <- known_freqs
-      return(return_val)
-    }
-  }  
-  
-  return(cur_res$res)
-}}
+      return_val[match(names(set_freqs), return_val$ancestry),"estimated_freq"] <- set_freqs
+      return(list(res = return_val,
+                  nll = boundary_res$nll,
+                  boundary = low_freq_bound,
+                  flag = "converged some freqs evaluated monomorphic")
+                  )
+    } else{ 
+      return(potential_return_val_converged)
+    } 
+
+}
 
 
 

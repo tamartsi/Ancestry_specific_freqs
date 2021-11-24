a161ddfbd48cf03c36a4105620a5b07cb4d88b22
Tamar Sofer <tamartsi@users.noreply.github.com>
2021-11-24 16:19:39 -0500

    created LAFA with known frequency, dynamic boundary

---
+++ Code/LAFA_dynamic_boundary.R
@@ -0,0 +1,230 @@
+# Function to estimate ancestry-specific allele frequencies for a given variant.
+# takes a vector of allele_counts allele counts for a given variant for n people 
+# and an n x d matrix prop_mat giving, for each of n people, d ancestral ancestry proportion
+# at the locus (could be genome-wide) for d ancestries. 
+# this is a wrapper function that changes the frequency boundaries and 
+# if frequencies for some ancestries are estimated at the set boundary conditions,
+# compares the likelihood to the likelihood when the frequencies are set at the 
+# boundaries of the parameter space (0 or 1, as needed).
+
+### LAFA update #### (need to update documentation above)
+## Tamar needs to continue editting from line 178
+
+LAFA_dynamic_boundary <- function(allele_counts, 
+                                  ancestry1, 
+                                  ancestry2 = NULL,
+                                  confidence = 0.95, 
+                                 frequency_boundary_grid = c(0.001, 0.01, 0.02, 0.05),
+                                 use_smoothing_data = TRUE,
+                                 chromosome_x = FALSE,
+                                 sex = NULL, 
+                                 male_label = "M", 
+                                 mac_filter = 5, 
+                                 ancestry_names = NULL){
+ 
+  
+  stopifnot(length(allele_counts) == length(ancestry1))
+  if (is.null(ancestry_names)) ancestry_names <- unique(c(ancestry1, ancestry2))
+  
+  # check for NAs, if there are observations with missing values, remove them.
+  inds_na_alleles <- which(is.na(allele_counts))
+  inds_na_ancestry1 <- which(is.na(ancestry1))
+  
+  inds_na <- c(inds_na_alleles, inds_na_ancestry1)
+  
+  if (!chromosome_x){
+    inds_na_ancestry2 <- which(is.na(ancestry2))
+    inds_na <- c(inds_na, inds_na_ancestry2)
+  }
+  
+  
+  if (length(inds_na) >0){
+    message(paste(length(inds_na), "observations with missing values, removing them..."))
+    allele_counts <- allele_counts[-inds_na]
+    ancestry1 <- ancestry1[-inds_na]
+    
+    if (!chromosome_x){
+      ancestry2 <- ancestry2[-inds_na]
+    }
+    
+  }
+  
+  if (chromosome_x){
+    stopifnot(length(sex) == length(allele_counts))
+    prep_dat <- .prep_dat_for_poisbin_likelihood_chr_x(allele_counts, 
+                                                       ancestry1,
+                                                       ancestry2,
+                                                       sex = sex, 
+                                                       male_label = male_label)
+    
+    ancestry1 <- prep_dat$ancestry1
+    ancestry2 <- prep_dat$ancestry2
+    allele_counts <- prep_dat$allele_counts
+    
+  }
+  
+  
+  
+  
+  
+  
+  # add made-up data to avoid boundaries of the frequency parameter space   
+  if (use_smoothing_data){
+    smoothing_data <- .generate_smoothing_observations_lafa(unique(c(ancestry1, ancestry2)))
+    allele_counts <- c(allele_counts, smoothing_data$simulated_allele_count)
+    ancestry1 <- c(ancestry1, smoothing_data$simulated_ancestry1)
+    
+    if (!is.null(ancestry2)){
+      ancestry2 <- c(ancestry2, rep(NA, length(smoothing_data$simulated_ancestry1)))
+    }
+  }
+  
+  # prepare data.frame for returning null results if the function doesn't run
+  # or does not converge
+  return_val_not_run <- data.frame(ancestry = ancestry_names,
+                                   estimated_freq = NA,
+                                   low_CI = NA,
+                                   high_CI = NA)
+  
+ 
+ 
+  
+  # start loop to find boundaries in which the analysis converges:
+  converged <- FALSE
+  ind_grid <- 1
+  while (!converged & ind_grid <= length(frequency_boundary_grid)){
+    low_freq_bound <- frequency_boundary_grid[ind_grid]
+    high_freq_bound <- 1 - frequency_boundary_grid[ind_grid]
+    ## optimize to estimate frequencies
+    
+    cur_res <- tryCatch({.estimate_frequencies_after_prep_pb(allele_counts, 
+                                                             ancestry1,
+                                                             ancestry2,
+                                                             low_freq_bound,
+                                                             high_freq_bound,
+                                                             confidence)},
+                        error = function(error){
+                          "not converged"
+                        }
+                      )
+    if (!identical(cur_res, "not converged")){
+      converged <- TRUE
+    } else{
+      ind_grid <- ind_grid + 1
+    }
+  }
+  
+  
+  if (!converged) {
+    return(list(res = return_val_not_run, 
+                nll = NA,
+                boundary = NA, 
+                flag = "Not converged"))
+  } else{ # if converged, prepare potential return value
+    cur_res$res <- cur_res$res[match(ancestry_names, cur_res$res$ancestry),]
+    cur_res$res$ancestry <- ancestry_names
+    potential_return_val_converged <- list(res = cur_res$res,
+                                           nll = cur_res$nll,
+                                           boundary = low_freq_bound,
+                                           flag = "converged")
+  }
+  
+  # check if convergence is right at the boundary condition for some ancestries
+  low_bound_inds <- which(cur_res$res$estimated_freq == low_freq_bound)
+  high_bound_inds <- which(cur_res$res$estimated_freq == high_freq_bound)
+  
+  # if nothing was estimated at the boundary, return the computed frequencies.
+  if (length(low_bound_inds) + length(high_bound_inds) == 0){
+    return(potential_return_val_converged)
+  }
+  
+  # otherwise, we continue to identify which frequencies were 
+  # estimated at the boundaries and...
+  # prepare a vector called set_freqs in which the ancestries
+  # where frequencies were estimated in the boundaries are set to the 
+  # boundaries of the parameter space. Start with low and then high boundary.
+    
+  # if frequencies for some ancestries were estimated right at the lower bound:
+  if (length(low_bound_inds) >0){
+      set_freqs <- rep(0, length = length(low_bound_inds))
+      names(set_freqs) <- cur_res$res$ancestry[low_bound_inds]
+    }
+  
+  # if frequencies for some ancestries were estimated right at the high bound:
+  if (length(high_bound_inds) >0 ){
+      set_freqs_high <- rep(1, length = length(high_bound_inds))
+      names(set_freqs_high) <- cur_res$res$ancestry[high_bound_inds]
+      
+      # should we augment a previous vector, and have a new set_freqs vector?
+      if (length(low_bound_inds) > 0){
+        set_freqs <- c(set_freqs, set_freqs_high)
+      } else{
+        set_freqs <- set_freqs_high
+      }
+    } # finished preparing a vector of set frequencies
+ 
+  # first: if all frequencies were estimated at the boundary:
+  if (length(set_freqs) == length(ancestry_names)){
+    allele_probs <- prop_mat[,names(set_freqs), drop = FALSE] %*% set_freqs
+    like_by_obs <- dbinom_approx(allele_counts, max_counts, allele_probs)
+    
+    # check if we have an impossible value (probability zero)
+    if (sum(like_by_obs == 0) > 0) return(potential_return_val_converged)
+    
+    nll <- -sum(log(like_by_obs))
+    
+    if (nll < potential_return_val_converged$nll){
+      return_val <- return_val_not_run
+      return_val[match(names(set_freqs), return_val$ancestry),"estimated_freq"] <- set_freqs
+      return(list(res = return_val,
+                  nll = nll,
+                  boundary = potential_return_val_converged$boundary,
+                  flag = "converged and evaluated monomorphic"))
+    } else{
+      return(potential_return_val_converged)
+    }
+  }
+  
+  # at this point: some frequencies but not all were estimated at the boundary
+  boundary_res <- 
+    tryCatch({.estimate_frequencies_w_known_freq_after_prep(allele_counts = allele_counts, 
+                                                  prop_mat = prop_mat, 
+                                                  max_counts = max_counts,
+                                                  known_freqs= set_freqs, 
+                                                  n_unknown = ncol(prop_mat) - length(set_freqs),
+                                                  n_known = length(set_freqs),
+                                                  low_freq_bound = low_freq_bound, 
+                                                  high_freq_bound = high_freq_bound,
+                                                  confidence = confidence)},
+             error = function(error){
+               "not converged"
+             }
+    )
+    # if this did not converge...
+    if (identical(boundary_res, "not converged")){
+     return(potential_return_val_converged)
+    }
+  
+    # if it did converge, continue...
+  
+    if (boundary_res$nll < cur_res$nll) {
+      # we need to return the new result, with the estimated 
+      # frequency for the ancestry with boundary value set to that value
+      return_val <- return_val_not_run
+      return_val[match(boundary_res$res$ancestry, return_val$ancestry),] <- boundary_res$res
+      return_val[match(names(set_freqs), return_val$ancestry),"estimated_freq"] <- set_freqs
+      return(list(res = return_val,
+                  nll = boundary_res$nll,
+                  boundary = low_freq_bound,
+                  flag = "converged some freqs evaluated monomorphic")
+                  )
+    } else{ 
+      return(potential_return_val_converged)
+    } 
+
+}
+
+
+
+
+
---
+++ Code/LAFA_w_known_freqs.R
@@ -0,0 +1,174 @@
+
+##
+# Function to estimate ancestry-specific allele frequencies for a given variant,
+# when some (one or more) ancestral allele frequencies are known and provided.
+# takes a vector of allele_counts allele counts for a given variant for n people 
+# and an n x d matrix matrix prop_mat giving, for each of n people, d ancestral ancestry proportion
+# at the locus (could be genome-wide) for d ancestries. 
+# known_freq need to be a named vector, specifying known frequencies. The names of elements 
+# in known_freq are some of the column names of prop_mat. 
+
+
+
+LAFA_w_known_freqs <- function(allele_counts, 
+                               ancestry1, 
+                               ancestry2 = NULL,
+                               known_freqs, 
+                               confidence = 0.95, 
+                               low_freq_bound = 0.001, high_freq_bound = 0.999,
+                               use_smoothing_data = FALSE,
+                               chromosome_x = FALSE,
+                               sex = NULL, 
+                               male_label = "M",
+                               mac_filter = 5,
+                               ancestry_names = NULL){
+  
+  stopifnot(length(allele_counts) == length(ancestry1))
+  if (is.null(ancestry_names)) ancestry_names <- unique(c(ancestry1, ancestry2))
+  
+  stopifnot(all(is.element(names(known_freqs), ancestry_names)))
+  stopifnot(length(known_freqs) > 0 & length(known_freqs) < length(ancestry_names))
+  
+
+  
+  # check for NAs, if there are observations with missing values, remove them.
+  inds_na_alleles <- which(is.na(allele_counts))
+  inds_na_ancestry1 <- which(is.na(ancestry1))
+  
+  inds_na <- c(inds_na_alleles, inds_na_ancestry1)
+  
+  if (!chromosome_x){
+    inds_na_ancestry2 <- which(is.na(ancestry2))
+    inds_na <- c(inds_na, inds_na_ancestry2)
+  }
+  
+  
+  if (length(inds_na) >0){
+    message(paste(length(inds_na), "observations with missing values, removing them..."))
+    allele_counts <- allele_counts[-inds_na]
+    ancestry1 <- ancestry1[-inds_na]
+    
+    if (!chromosome_x){
+      ancestry2 <- ancestry2[-inds_na]
+    }
+    
+  }
+  
+  if (chromosome_x){
+    stopifnot(length(sex) == length(allele_counts))
+    prep_dat <- .prep_dat_for_poisbin_likelihood_chr_x(allele_counts, 
+                                                       ancestry1,
+                                                       ancestry2,
+                                                       sex = sex, 
+                                                       male_label = male_label)
+    
+    ancestry1 <- prep_dat$ancestry1
+    ancestry2 <- prep_dat$ancestry2
+    allele_counts <- prep_dat$allele_counts
+    
+  }
+  
+  
+  # check if the number of minor alleles is higher than the mac_filter,
+  # stop if MAC is too low.
+  if (!chromosome_x){
+    max_alleles <- 2*length(allele_counts)  
+  } else{ # chromosome x
+    n_male <- sum(sex == male_label)
+    n_all <- length(allele_counts)
+    max_alleles <- n_male + 2*(n_all - n_male)
+  }
+  stopifnot(min(sum(allele_counts), 
+                max_alleles - sum(allele_counts)) > mac_filter)    
+  
+  
+  # add made-up data to avoid boundaries of the frequency parameter space   
+  if (use_smoothing_data){
+    smoothing_data <- .generate_smoothing_observations_lafa(setdiff(ancestry_names, names(known_freqs)))
+    allele_counts <- c(allele_counts, smoothing_data$simulated_allele_count)
+    ancestry1 <- c(ancestry1, smoothing_data$simulated_ancestry1)
+    
+    if (!is.null(ancestry2)){
+      ancestry2 <- c(ancestry2, rep(NA, length(smoothing_data$simulated_ancestry1)))
+    }
+  }
+  
+
+  
+  out <- .LAFA_w_known_freq_after_prep(allele_counts, 
+                                       ancestry1, 
+                                       ancestry2 = NULL,
+                                       known_freqs, 
+                                       n_unknown,
+                                       n_known,
+                                       low_freq_bound, 
+                                       high_freq_bound,
+                                       confidence)
+  out$res <- out$res[match(ancestry_names, out$res$ancestry),]
+  out$res$ancestry <- ancestry_names
+  return(out)
+  
+}
+
+
+
+.LAFA_w_known_freq_after_prep <- function(allele_counts, 
+                                         ancestry1, 
+                                         ancestry2 = NULL,
+                                         known_freqs, 
+                                         n_unknown,
+                                         n_known,
+                                         low_freq_bound, 
+                                         high_freq_bound,
+                                         confidence){
+  
+  ancestry_names <- na.omit(unique(c(ancestry1, ancestry2)))
+  unknown_ancestry_names <- setdiff(ancestry_names, names(known_freqs))
+  n_ancestry <- length(ancestry_names)
+  n_unknown <- length(unknown_ancestry_names)
+ 
+  ## compute the negative log likelihood function
+  
+  nll <- function(unknown_freqs){
+    names(unknown_freqs) <- unknown_ancestry_names
+    freqs <- c(known_freqs, unknown_freqs)
+    prob1 <- freqs[ancestry1]
+    if (!is.null(ancestry2)){
+      prob2 <- freqs[ancestry2]
+    }  else{
+      prob2 <- NULL
+    }
+    # prob1 and prob2 should be a function of freqs...
+    nll_by_obs <- log(dpoisbinom_approx(allele_counts, prob1, prob2))
+    return(-sum(nll_by_obs))		
+  }
+  
+  
+  
+  ## optimize to obtain allele frequency estimates
+  
+  if (n_unknown == 1){
+    fit <- optim(par = rep(0.5, n_unknown), fn = nll, hessian = TRUE,
+                 method = "Brent", lower = low_freq_bound, upper = high_freq_bound)
+  } else{
+    fit <- optim(par = rep(0.5, n_unknown), fn = nll, hessian = TRUE,
+                 lower = rep(low_freq_bound, n_unknown), 
+                 upper = rep(high_freq_bound, n_unknown), 
+                 method = "L-BFGS-B")
+  }
+  
+  
+  estimated_freqs <- fit$par
+  hessian <- fit$hessian
+  ses <- sqrt(diag(solve(hessian)))
+  res <- data.frame(ancestry = unknown_ancestry_names, 
+                    estimated_freq = estimated_freqs, 
+                    low_CI = estimated_freqs - ses*sqrt(qchisq(confidence, 1)), 
+                    high_CI= estimated_freqs + ses*sqrt(qchisq(confidence, 1)))
+  
+  # return the estimated frequencies, and the negative log likelihood. 
+  return(list(res = res, nll = nll(res$estimated_freq)))
+  
+}
+
+
